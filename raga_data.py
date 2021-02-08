1720a615d14b57d48b2823d3235883793eacdab9
thekoc11 <thekoc11@gmail.com>
2021-02-09 00:15:10 +0530

    Add Mayamalagowla and Malahari indstead of sankarabharanam and bilahari

--- raga_data.py
+++ raga_data.py
@@ -1,13 +1,6 @@
-SAMI_NINNE = {"melody": [], "durations": [], "beat_onset": [], 'rests': []}
+KSHEERA_SAGARA = {"melody": [], "durations": [], "beat_onset": [], 'rests': []}
 
-SAMI_NINNE["melody"] = []
+PANCHA_MATANGE = {"melody": [], "durations": [], "beat_onset": [], 'rests': []}
 
-BHAKTI_BICCHA = {"melody": [], "durations": [], "beat_onset": [], 'rests': []}
+MERU_SAMANA = {"melody": [], "durations": [], "beat_onset": [], 'rests': []}
 
-BHAKTI_BICCHA["melody"] = [
-    99999, 5, 5, 4, 2, 0, 99999, 0,
-    2, 4, 2, 7, 5, 5, 4, 99999, 99999,
-    99999, 5, 5, 4, 2, 5, 4, 2, 0, -1,
-    2, 4, 2, 7, 7, 5, 2, 4, 99999, 99999,
-
-]
